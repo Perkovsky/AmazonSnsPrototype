20b6f0a4db97169c10a0cfb0bdc7b69e4560e879
Igor Perkovsky <igor.perkovsky@mail.ru>
2021-02-23 09:57:20 +0200

    Basic logic has been added

--- AmazonSnsPrototype/Program.cs
+++ AmazonSnsPrototype/Program.cs
@@ -1,18 +1,42 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Threading.Tasks;
 
 namespace AmazonSnsPrototype
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
 			var config = new ConfigurationBuilder()
 				.AddUserSecrets<AmazonSnsSettings>()
 				.Build();
-			var settings = config.GetSection("AmazonSNS").Get<AmazonSnsSettings>();
+			
+            var settings = config.GetSection("AmazonSNS").Get<AmazonSnsSettings>();
+            Console.WriteLine($"TopicArn : '{settings.TopicArn}'.");
 
-			Console.WriteLine($"AmazonSNS:Url = '{settings.Url}'.");
+            string message = $"This test message was sent at {DateTime.Now}.";
+            Console.WriteLine($"Message  : '{message}'.");
+
+            var client = new AmazonSimpleNotificationServiceClient(region: RegionEndpoint.USWest1);
+            Console.WriteLine();
+
+            try
+            {
+                var response = await client.PublishAsync(new PublishRequest
+                {
+                    Message = message,
+                    TopicArn = settings.TopicArn
+                });
+                Console.WriteLine($"Message has been sent to topic.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 		}
 	}
 }
